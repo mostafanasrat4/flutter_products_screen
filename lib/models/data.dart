d45ee226827c2fa1f84be86cf240dbdc64b97956
mostafanasrat4 <mostafanasrat4@gmail.com>
2022-08-11 19:15:15 +0200

    TabBar Added

---
+++ lib/models/data.dart
@@ -0,0 +1,95 @@
+class AppData {
+  static List women_products = [
+    {
+      'title': 'New',
+      'imageUrl':
+          'https://s3-alpha-sig.figma.com/img/715c/827c/e012b7c12e4b2a5bc61b8683ec894a9b?Expires=1661126400&Signature=ds~E0IMJBZcZACy-d8B5P5ftOSiKRKdd6LYxxFt1JjE~KJH~LTxML6R2bNkMGRdaGhpwxaKyF3sZWlcCI6f2PW6PdkzdUdTi5rlbExJwx7u~JEpbd2AhekBtUG7Spz9dRFkLqUSjOazz9wixx1uvK3ZxwAhd0V8paV3CeCHg6VbY0J92dBf-vsCqqfUuteeE95cdV8O8DkDCM8yk2mKJKnkSIG08rPEY7rZlxUXIGD~~4~TW6LSwadhYVVJ9AUIE-M4P8iK1vjlyurwPglRjdy34wfpdtZ96Pt~hoXHYDEKYKVaV-NiQKkAJ3B8PJum-qH3KjixsU1FvzC8RR4LNJg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+    },
+    {
+      'title': 'Clothes',
+      'imageUrl':
+          'https://s3-alpha-sig.figma.com/img/020f/c601/8d2de9e19bc84c5911cf56db5516230c?Expires=1661126400&Signature=e10X8G8O-fmT9y2raBx1IaXb94MN56zzjW6E~ANTkgUDgLt1z6lmZ25ZFLw0SyNXHiUBkwqE9ndTXI7awzoBhVXKpWlMmnS54h6o5rfaH7nsrejNyUGdMXQ9~d~h3kqBRWqeG9~YVwuOdYwTpdPDkEiSd~VSblYZQz6cf1beBIitx~qkMXPzeW88rCOswAMgz7~wsQjEgOlb6beUncPc~JhP3T9jyjR4aawqIMwmeXi079vZtQG7S6ka6Eq43Zq6PB06hythtBJcV3N1uK8bRDFKN0R013MHzAyBvMrci80IHeXFCWwfJ9rs8kU-qRwVzUmeDAO4r7j3gzAn~a~w7g__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+    },
+    {
+      'title': 'Shoes',
+      'imageUrl':
+          'https://s3-alpha-sig.figma.com/img/ef74/9cf9/46b19562fcd50e353f20c0b75c070865?Expires=1661126400&Signature=UBvQe01getfEnzo3LHogdXkWQuFM9Qxwakmp84Fplfrsl3G1YmNYw8dQdFs82ai6lLatK9hk1pa1~2slrZyHIudCwkt1r6CKPMW4YDzDMXAR5TruYTosNCoOsgU~CtvRfovWLtCgH1zHyzkFsLVOCUz~oRYY8f8SzkLwMNmIG8F1FM6XiR5EH5-kIarYDfFTledYhccUR37aPzH5b39fT4tQsSgxp1SzSoXToLYa~FVXKYQFBsfjkFm15rZ2kQiiEEmzJImSlw3QR6FWk3aJsZjA4nyEp9XGS16y5vqJERBxZmXcahEqtdRNTMMFdf1Iou2FHWGYth5cQe-h~PuTRQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+    },
+    {
+      'title': 'Accesories',
+      'imageUrl':
+          'https://s3-alpha-sig.figma.com/img/5bfc/9b2c/577233e2c04f0024ec9ea69388017eaf?Expires=1661126400&Signature=esQP~6rPACgGBM9y0fpbJOjr8qYEFzFT485ilijrimrruh8kOYULDC-yRMazVyYpWSkJdngqZGQfJoCWfx4Z6Y41umZIzStJ0X8xJsQG0vWXidFW1A3vh3KOzoyA-9~L1Tji18ZAVD5WcEyzUbsOJMO1wk2V1QjZ22E4BNsZ-db-7HGbTSPM7fxFDKNEjrDpeOFxk9VLCSH65eTtg69UfBmcWBIGOJ9hSOJqY8yc8Bi2gz0-qBtMH5P8ZQFQMfhKLGSywXUsn77dgR3-d5MSB88CcAvxPrT2J22YGBI~34XUnwz3oF~U-pqQL9mtyXGimJ3pCOFY9v6KsygL0rOHcw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+    },
+    {
+      'title': 'New',
+      'imageUrl':
+          'https://s3-alpha-sig.figma.com/img/715c/827c/e012b7c12e4b2a5bc61b8683ec894a9b?Expires=1661126400&Signature=ds~E0IMJBZcZACy-d8B5P5ftOSiKRKdd6LYxxFt1JjE~KJH~LTxML6R2bNkMGRdaGhpwxaKyF3sZWlcCI6f2PW6PdkzdUdTi5rlbExJwx7u~JEpbd2AhekBtUG7Spz9dRFkLqUSjOazz9wixx1uvK3ZxwAhd0V8paV3CeCHg6VbY0J92dBf-vsCqqfUuteeE95cdV8O8DkDCM8yk2mKJKnkSIG08rPEY7rZlxUXIGD~~4~TW6LSwadhYVVJ9AUIE-M4P8iK1vjlyurwPglRjdy34wfpdtZ96Pt~hoXHYDEKYKVaV-NiQKkAJ3B8PJum-qH3KjixsU1FvzC8RR4LNJg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+    },
+    {
+      'title': 'Clothes',
+      'imageUrl':
+          'https://s3-alpha-sig.figma.com/img/020f/c601/8d2de9e19bc84c5911cf56db5516230c?Expires=1661126400&Signature=e10X8G8O-fmT9y2raBx1IaXb94MN56zzjW6E~ANTkgUDgLt1z6lmZ25ZFLw0SyNXHiUBkwqE9ndTXI7awzoBhVXKpWlMmnS54h6o5rfaH7nsrejNyUGdMXQ9~d~h3kqBRWqeG9~YVwuOdYwTpdPDkEiSd~VSblYZQz6cf1beBIitx~qkMXPzeW88rCOswAMgz7~wsQjEgOlb6beUncPc~JhP3T9jyjR4aawqIMwmeXi079vZtQG7S6ka6Eq43Zq6PB06hythtBJcV3N1uK8bRDFKN0R013MHzAyBvMrci80IHeXFCWwfJ9rs8kU-qRwVzUmeDAO4r7j3gzAn~a~w7g__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+    },
+  ];
+
+  static List men_products = [
+    {
+      'title': 'New',
+      'imageUrl':
+          'https://m.media-amazon.com/images/I/61OU7v1h0xL._AC_UL480_QL65_.jpg',
+    },
+    {
+      'title': 'Clothes',
+      'imageUrl':
+          'https://images-eu.ssl-images-amazon.com/images/G/42/SL/April/XCM_Manual_1421693_4631433_Egypt_EG_SL_L2_revamp_EN_Sportswear_794x1238_2X.jpg'
+    },
+    {
+      'title': 'Shoes',
+      'imageUrl':
+          'https://images-eu.ssl-images-amazon.com/images/G/42/SL/April/XCM_Manual_1421693_4631422_Egypt_EG_SL_L2_revamp_EN_Shoes_794x1238_2X.jpg'
+    },
+    {
+      'title': 'Accesories',
+      'imageUrl':
+          'https://m.media-amazon.com/images/I/51AR0pk8k-L._AC_UL480_QL65_.jpg'
+    },
+    {
+      'title': 'New',
+      'imageUrl':
+          'https://m.media-amazon.com/images/I/61OU7v1h0xL._AC_UL480_QL65_.jpg'
+    },
+    {
+      'title': 'Clothes',
+      'imageUrl':
+          'https://images-eu.ssl-images-amazon.com/images/G/42/SL/April/XCM_Manual_1421693_4631433_Egypt_EG_SL_L2_revamp_EN_Sportswear_794x1238_2X.jpg'
+    },
+  ];
+
+  static List kids_products = [
+    {
+      'title': 'New',
+      'imageUrl':
+          'https://images-eu.ssl-images-amazon.com/images/G/42/Sunrise/SL/XCM_Manual_1323197_1661000_Egypt_SR_EG_CP_NT_SL_KidsMainPage_BoyTops_3868335_440x440_en_AE.jpg'
+    },
+    {
+      'title': 'Clothes',
+      'imageUrl':
+          'https://images-eu.ssl-images-amazon.com/images/G/42/Sunrise/SL/XCM_Manual_1323197_1661000_Egypt_SR_EG_CP_NT_SL_KidsMainPage_GirlTops_3868295_440x440_en_AE.jpg'
+    },
+    {
+      'title': 'Shoes',
+      'imageUrl':
+          'https://images-eu.ssl-images-amazon.com/images/G/42/Sunrise/SL/XCM_Manual_1323197_1661000_Egypt_SR_EG_CP_NT_SL_KidsMainPage_BoyFootwear_3868333_440x440_en_AE.jpg'
+    },
+    {
+      'title': 'BackPacks',
+      'imageUrl':
+          'https://images-eu.ssl-images-amazon.com/images/G/42/Sunrise/SL/XCM_Manual_1323197_1661000_Egypt_SR_EG_CP_NT_SL_KidsMainPage_BoyBackpacks_3868330_440x440_en_AE.jpg'
+    },
+    {
+      'title': 'Pants',
+      'imageUrl':
+          'https://images-eu.ssl-images-amazon.com/images/G/42/Sunrise/SL/XCM_Manual_1323197_1661000_Egypt_SR_EG_CP_NT_SL_KidsMainPage_GirlPants_3868329_440x440_en_AE.jpg'
+    },
+  ];
+}
